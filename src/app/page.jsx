d4487f0bb0cc655cd4d463ccd4b2bf86477131ac
DynamicDOMinator <ahmedsayedhamed38@gmail.com>
2025-10-05 06:38:43 +0300

    SADFasf

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,9 +16,9 @@ export default function Home() {
       <Testimonials />
  
       <Experiences />
+      <Pricing />
       <Transformations />
       <Journey />
-      <Pricing />
       <FAQs />
     </div>
   );
